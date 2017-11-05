5ac9f1fdb60b510dd048d50f7c40f6e27d6ea0e6
Bartek <b.janczak90@gmail.com>
2017-11-05 16:01:50 +0100

    +

--- src/main/java/com/mojafirma/Main.java
+++ src/main/java/com/mojafirma/Main.java
@@ -1,6 +1,23 @@
 package com.mojafirma;
 
 
-public class Main {
+import com.mojafirma.implement.UserDaoImp;
+import com.mojafirma.interfaces.UserDao;
+import com.mojafirma.model.User;
 
+public class Main {
+    public static void main(String[] args) {
+
+        UserDaoImp userDaoImp = new UserDaoImp();
+        User user = new User();
+        user.setName("jan");
+        user.setSurname("kowalski");
+
+        User user1 = new User();
+        user1.setName("andrzej");
+        user1.setSurname("kowalski");
+
+        userDaoImp.insertUser(user);
+        userDaoImp.insertUser(user1);
+    }
 }
